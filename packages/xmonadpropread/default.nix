585890235a987a0a6b23676278908237d708ad6d
gamoutatsumi <tatsumi@gamou.dev>
2024-10-01 13:17:40 +0900

    fix

--- packages/xmonadpropread/default.nix
+++ packages/xmonadpropread/default.nix
@@ -7,7 +7,15 @@ let
   haskellPackages = pkgs.haskell.packages."${ghcBuild}";
   pkg = haskellPackages.developPackage {
     root = ./.;
-    modifier = drv: pkgs.haskell.lib.addBuildTools drv (with haskellPackages; [ cabal-install ]);
+    modifier =
+      drv:
+      pkgs.haskell.lib.compose.overrideCabal
+        (drv: pkgs.haskell.lib.addBuildTools drv (with haskellPackages; [ cabal-install ]))
+        {
+          meta = {
+            platforms = [ "x86_64-linux" ];
+          };
+        };
   };
 in
 pkg
