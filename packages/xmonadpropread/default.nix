1db50ab18c575eef4b8d57fb2baabba845787429
gamoutatsumi <tatsumi@gamou.dev>
2024-10-01 13:19:02 +0900

    fix

--- packages/xmonadpropread/default.nix
+++ packages/xmonadpropread/default.nix
@@ -10,7 +10,7 @@ let
     modifier =
       drv:
       pkgs.haskell.lib.compose.overrideCabal
-        (drv: pkgs.haskell.lib.addBuildTools drv (with haskellPackages; [ cabal-install ]))
+        (pkgs.haskell.lib.addBuildTools drv (with haskellPackages; [ cabal-install ]))
         {
           meta = {
             platforms = [ "x86_64-linux" ];
