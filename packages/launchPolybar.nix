cc1668142ba263b649c42c3715b939cc224b6288
gamoutatsumi <tatsumi@gamou.dev>
2024-10-26 00:53:05 +0900

    update

--- packages/launchPolybar.nix
+++ packages/launchPolybar.nix
@@ -8,6 +8,7 @@ pkgs.writeShellApplication {
     pkgs.coreutils
     pkgs.iproute2
     pkgs.polybarFull
+    (pkgs.callPackage ./xmonadpropread.nix)
   ];
   derivationArgs = {
     meta.platforms = [ "x86_64-linux" ];
