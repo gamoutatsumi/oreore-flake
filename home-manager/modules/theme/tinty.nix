8a34c35f4a0464c1e70b248f67ab443eba89391d
gamoutatsumi <tatsumi@gamou.dev>
2024-11-27 17:22:51 +0900

    update

--- home-manager/modules/theme/tinty.nix
+++ home-manager/modules/theme/tinty.nix
@@ -102,7 +102,7 @@ in
               ''
                 mkdir -p $out/repos
                 cp -r ${tintySchemes} $out/repos/schemes
-                tinty generate-scheme --config ${cfgFile} --data-dir $out --system base24 --name 'Wallpaper' --variant ${cfg.generate.variant} --save ${config.theme.wallpaper.file}
+                tinty generate-scheme --config ${cfgFile} --data-dir $out --system base24 --name 'Wallpaper' --slug 'wallpaper' --variant ${cfg.generate.variant} --save ${config.theme.wallpaper.file}
                 tinty install --config ${cfgFile} --data-dir $out
                 tinty list --config ${cfgFile} --custom-schemes --data-dir $out
                 tinty apply --config ${cfgFile} --data-dir $out ${cfg.scheme}
