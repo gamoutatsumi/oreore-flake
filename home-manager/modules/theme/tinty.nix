db9e9866daa7b85239a2ca20a6d43d320dec5376
gamoutatsumi <tatsumi@gamou.dev>
2024-11-27 13:41:00 +0900

    update

--- home-manager/modules/theme/tinty.nix
+++ home-manager/modules/theme/tinty.nix
@@ -7,7 +7,27 @@
 let
   cfg = config.theme.tinty;
   settingsFormat = pkgs.formats.toml { };
-  cfgFile = settingsFormat.generate "config.toml" cfg.settings;
+  genCfgFile = settings: settingsFormat.generate "config.toml" (settings // cfg.settings);
+  cfgFile = genCfgFile {
+    shell = "${cfg.shell} -c '{}'";
+    default-scheme = cfg.scheme;
+  };
+  itemType = lib.types.submodule {
+    options = {
+      name = lib.mkOption {
+        type = lib.types.str;
+      };
+      path = lib.mkOption {
+        type = lib.types.path;
+      };
+      themes-dir = lib.mkOption {
+        type = lib.types.path;
+      };
+      hooks = lib.mkOption {
+        type = lib.types.str;
+      };
+    };
+  };
   tintyType = lib.types.submodule {
     options = {
       enable = lib.mkEnableOption "Enable tinty for Tinted-Theming (base16 / base24)";
@@ -16,12 +36,26 @@ let
         default = { };
       };
       package = lib.mkPackageOption pkgs "tinty" { };
-      variant = lib.mkOption {
-        type = lib.types.enum [
-          "light"
-          "dark"
-        ];
-        default = "light";
+      shell = lib.mkOption {
+        type = lib.types.str;
+        default = "bash";
+      };
+      scheme = lib.mkOption {
+        type = lib.types.str;
+        default = if config.theme.wallpaper.file != null then "base24-wallpaper" else "base16-mocha";
+      };
+      generate = {
+        variant = lib.mkOption {
+          type = lib.types.enum [
+            "light"
+            "dark"
+          ];
+          default = "light";
+        };
+      };
+      items = lib.mkOption {
+        type = lib.types.list itemType;
+        default = [ ];
       };
     };
   };
@@ -51,7 +85,7 @@ in
               }
               ''
                 export XDG_DATA_HOME=$(mktemp -d)
-                            tinty generate-scheme --config ${cfgFile} --system base24 --name Wallpaper --slug wallpaper --variant ${cfg.variant} ${config.theme.wallpaper.file} > $out
+                            tinty generate-scheme --config ${cfgFile} --system base24 --name 'Base24 Wallpaper' --slug base24-wallpaper --variant ${cfg.generate.variant} ${config.theme.wallpaper.file} > $out
               '';
         };
       };
