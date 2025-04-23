0e9d1d7c5ecde60559775624aa35282af6b4df9a
gamoutatsumi <tatsumi@gamou.dev>
2025-04-23 16:48:22 +0900

    update

--- flake.nix
+++ flake.nix
@@ -3,30 +3,13 @@
 
   inputs = {
     # keep-sorted start block=yes
-    fenix = {
-      url = "https://flakehub.com/f/nix-community/fenix/0.1.*";
-      inputs = {
-        nixpkgs = {
-          follows = "nixpkgs";
-        };
-      };
-    };
     flake-checker = {
       url = "github:DeterminateSystems/flake-checker";
       inputs = {
         nixpkgs = {
           follows = "nixpkgs";
         };
-        fenix = {
-          follows = "fenix";
-        };
-        naersk = {
-          follows = "naersk";
-        };
       };
-    };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
     };
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
@@ -36,36 +19,17 @@
         };
       };
     };
-    naersk = {
-      url = "https://flakehub.com/f/nix-community/naersk/0.1.*";
-      inputs = {
-        nixpkgs = {
-          follows = "nixpkgs";
-        };
-      };
-    };
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-24.11";
+      url = "github:NixOS/nixpkgs?shallow=1&ref=nixos-unstable";
     };
-    nixpkgs-unstable = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable = {
+      url = "github:NixOS/nixpkgs?shallow=1&ref=release-24.11";
     };
     pre-commit-hooks = {
       url = "github:cachix/git-hooks.nix";
       inputs = {
         nixpkgs = {
-          follows = "nixpkgs-unstable";
-        };
-        flake-compat = {
-          follows = "flake-compat";
-        };
-      };
-    };
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs = {
-          follows = "nixpkgs-unstable";
+          follows = "nixpkgs";
         };
       };
     };
@@ -92,7 +56,6 @@
       flake-parts,
       systems,
       flake-checker,
-      rust-overlay,
       tinty-schemes,
       ...
     }@inputs:
@@ -131,16 +94,21 @@
           }:
           let
             treefmtBuild = config.treefmt.build;
+            spkgs = import inputs.nixpkgs-stable {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
           in
           {
             _module = {
               args = {
-                pkgs = import inputs.nixpkgs-unstable {
+                pkgs = import inputs.nixpkgs {
                   inherit system;
                   config = {
                     allowUnfree = true;
                   };
-                  overlays = [ rust-overlay.overlays.default ];
                 };
               };
             };
@@ -150,9 +118,9 @@
               // lib.attrsets.optionalAttrs pkgs.stdenv.isLinux (import ./packages/linux { inherit pkgs lib; });
             overlayAttrs = self.packages."${system}";
             devShells = {
-              default = pkgs.mkShell {
+              default = spkgs.mkShell {
                 PFPATH = "${
-                  pkgs.buildEnv {
+                  spkgs.buildEnv {
                     name = "zsh-comp";
                     paths = config.devShells.default.nativeBuildInputs;
                     pathsToLink = [ "/share/zsh" ];
